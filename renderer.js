bbad18ffcf17cea3168c7eb58c29db7bcc22d91c
沫烬染 <mojinran@outlook.com>
2023-06-23 09:00:20 +0800

    优化代码，更新版本

--- renderer.js
+++ renderer.js
@@ -15,6 +15,7 @@ export function onLoad() {
     const doc = parser.parseFromString(window_on_top_btn, "text/html");
     const node = doc.querySelector("body > div");
 
+    // 添加点击按钮事件
     node.addEventListener("click", async () => {
         const resule = await window.window_on_top.toggleWindowOnTop();
         if (resule) {
@@ -24,33 +25,24 @@ export function onLoad() {
         }
     });
 
+    // 查找窗口控制区域，并插入按钮
     const interval = setInterval(() => {
         const window_control_area = document.querySelector(".window-control-area");
+        const setting_btn = document.querySelector(".login-container .setting");
 
         if (window_control_area) {
             clearInterval(interval);
-        }
 
-        try {
             // 获取窗口控制区域
             const childNodes = window_control_area.childNodes;
+
+            // 留出位置
+            if (setting_btn) {
+                setting_btn.style.marginRight = "32px";
+            }
+
             // 插入
             window_control_area.insertBefore(node, childNodes[0]);
-
-            // 获取两个按钮元素
-            let button1 = document.querySelector('.setting[data-v-6afc84c4]');
-            let button2 = document.querySelector('div[aria-label="置顶"]');
-
-            // 获取按钮的位置和尺寸
-            let rect1 = button1.getBoundingClientRect();
-            let rect2 = button2.getBoundingClientRect();
-
-            // 计算目标位置
-            let targetLeft = rect2.left - rect1.width;
-
-            // 设置按钮的位置
-            button1.style.position = 'absolute';
-            button1.style.left = targetLeft + 'px';
-        } catch (error) { }
+        }
     }, 100);
 }
