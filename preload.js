f8adffc0a6b159af2ebe1b07eeb78f7b1309abbf
FW27623 <1970446279@qq.com>
2023-07-11 19:30:19 +0800

    修复打开图片界面置顶按钮不可见的BUG

--- preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-
-contextBridge.exposeInMainWorld("window_on_top", {
-    toggleWindowOnTop: () => ipcRenderer.invoke("betterQQNT.window_on_top.toggleWindowOnTop"),
-});
